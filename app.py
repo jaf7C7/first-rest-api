94ef01bca6b4a61d8a80f99768b77f333cbb5d64
jfox <jfox@rhel9.home>
2023-11-02 15:14:46 +0000

    Add /stores endpoint to GET and POST stores

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from db import stores
+import uuid
 
 app = Flask(__name__)
 
@@ -7,10 +9,25 @@ app = Flask(__name__)
 def index():
     return {
         'endpoints': [
-            {'endpoint': '/echo', 'description': 'Echoes POSTed JSON back to the sender'}
+            {'endpoint': '/echo', 'description': 'Echoes POSTed JSON back to the sender'},
+            {'endpoint': '/stores', 'description': 'Returns data on all stores'}
         ]
     }, 200
 
 @app.post('/echo')
 def echo():
     return request.get_json(), 200
+
+
+@app.get('/stores')
+def get_all_stores():
+    return {'stores': list(stores.values())}, 200
+
+
+@app.post('/stores')
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {'id': store_id, **store_data}
+    stores[store_id] = new_store
+    return new_store, 201
